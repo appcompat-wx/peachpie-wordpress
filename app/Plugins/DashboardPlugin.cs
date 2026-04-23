f6b46f43f170aa50ee03a50c4fb83887a2360858
appcompat-wx <appcompatwx@microsoft.com>
2026-04-23 17:25:40 +0800

    upload retarget code from lab

---
+++ app/Plugins/DashboardPlugin.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc.ViewEngines;
+using System.Threading;
+using System.Threading.Tasks;
+using Pchp.Core;
+using Peachpie.AspNetCore.Mvc;
+using PeachPied.WordPress.Standard;
+
+namespace PeachPied.Demo.Plugins
+{
+    public class DashboardPlugin : IWpPlugin
+    {
+        public string Title { get; } = "Dashboard Widget";
+
+        public DashboardPlugin()
+        {
+        }
+
+        public ValueTask ConfigureAsync(WpApp app, CancellationToken cancellation)
+        {
+            app.DashboardWidget("peachpied.widget.1", "Razor Partial View", writer =>
+            {
+                app.Context.RenderPartial("DashboardWidget", this);
+            });
+
+            return ValueTask.CompletedTask;
+        }
+    }
+}
---
+++ app/Plugins/ShortcodePlugin.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using Microsoft.AspNetCore.Mvc.ViewEngines;
+using System.Threading;
+using System.Threading.Tasks;
+using Pchp.Core;
+using Peachpie.AspNetCore.Mvc;
+using Peachpie.AspNetCore.Web;
+using PeachPied.WordPress.Standard;
+
+namespace PeachPied.Demo.Plugins
+{
+    class ShortcodePlugin : IWpPlugin
+    {
+        public ShortcodePlugin()
+        {
+        }
+
+        public ValueTask ConfigureAsync(WpApp app, CancellationToken cancellation)
+        {
+            app.AddShortcode("dotnetWpUser", new Func<string>(() => //new shortcode_handler((attrs, content) =>
+            {
+                var user = (WP_User)app.Context.Call("wp_get_current_user", Array.Empty<PhpValue>()).AsObject();
+                if (user != null)
+                {
+                    return $"Your WP_User ID is {user.ID}.";
+                }
+                else
+                {
+                    return "You are not logged in.";
+                }
+            }));
+
+            return ValueTask.CompletedTask;
+        }
+    }
+}
